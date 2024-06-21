9d104e408e85888ac69414c253f67b79d0b52b19
Lis <l.ilya@outlook.com>
2024-06-21 13:25:35 +0300

    0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,16 @@
 use std::fmt;
 use std::fmt::{Debug, Formatter};
+
 use rand::Rng;
+use regex::Regex;
+
 use crate::error::RollError;
 
 mod tests;
 mod error;
 
 #[derive(Debug, Clone)]
-pub enum Dice {
-    D2 = 2,
-    D4 = 4,
-    D6 = 6,
-    D10 = 10,
-    D20 = 20,
-    D100 = 100,
-}
-
-#[derive(Debug, Clone)]
-pub enum Operation {
+enum Operation {
     Subst,
     Add,
     Mul,
@@ -25,15 +18,16 @@ pub enum Operation {
 }
 
 #[derive(Debug, Clone)]
-pub struct Expression {
+struct Expression {
     number: u8,
-    dice: Dice,
+    dice: u8,
     modifiers: Vec<(Operation, i16)>,
 }
 
 #[derive(Debug, Clone)]
 pub struct RollResult {
-    dice: Dice,
+    number: u8,
+    dice: u8,
     value: Vec<u32>,
     sum: i32,
 }
@@ -41,22 +35,25 @@ pub struct RollResult {
 impl fmt::Display for RollResult {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         for i in self.value.iter() {
-            writeln!(f, "{} -> {}", self.dice, i)?;
+            writeln!(f, "d{} -> {}", self.dice, i)?;
         }
         writeln!(f, "{}", self.sum)
     }
 }
 
-impl fmt::Display for Dice {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self {
-            Dice::D2 => { write!(f, "d2") }
-            Dice::D4 => { write!(f, "d4") }
-            Dice::D6 => { write!(f, "d6") }
-            Dice::D10 => { write!(f, "d10") }
-            Dice::D20 => { write!(f, "d20") }
-            Dice::D100 => { write!(f, "d100") }
+pub trait FancyDisplay {
+    fn to_fancy_str(&self) -> String;
+}
+
+impl FancyDisplay for RollResult {
+    fn to_fancy_str(&self) -> String {
+        let mut s = String::new();
+        s.push_str(format!("{}d{}\n", self.number, self.dice).as_str());
+        for i in self.value.iter() {
+            s.push_str(format!("-> {}\n", i).as_str());
         }
+        s.push_str(format!("____\n{}", self.sum).as_str());
+        s
     }
 }
 
@@ -69,11 +66,11 @@ pub fn roll_str(expression: &str) -> Result<RollResult, RollError> {
     roll(&expr)
 }
 
-pub fn roll(expr: &Expression) -> Result<RollResult, RollError> {
+fn roll(expr: &Expression) -> Result<RollResult, RollError> {
     let mut rng = rand::thread_rng();
     let mut result: Vec<u32> = vec![];
     for _i in 0..expr.number {
-        let n = rng.gen_range(1..=(expr.dice.clone() as u8));
+        let n = rng.gen_range(1..=expr.dice);
         result.push(n as u32);
     }
     let mut sum: i32 = result.iter().sum::<u32>() as i32;
@@ -86,12 +83,54 @@ pub fn roll(expr: &Expression) -> Result<RollResult, RollError> {
         };
     }
     Ok(RollResult {
-        dice: expr.dice.clone(),
+        number: expr.number,
+        dice: expr.dice,
         value: result,
         sum,
     })
 }
 
 fn parse(expression: &str) -> Option<Expression> {
-    None
+    let re1 = Regex::new(r"(?<num>\d*)d(?<dice>\d+)(?<mods>.*)").unwrap();
+    let re2 = Regex::new(r"[+\-*/]\d+").unwrap();
+
+    let Some(result) = re1.captures(expression) else {
+        return None
+    };
+
+    let mods_option: Option<Vec<(Operation, i16)>> = match result.name("mods") {
+        None => {
+            None
+        }
+        Some(mods) => {
+            re2.find_iter(mods.as_str()).map(|m| {
+                let str = m.as_str();
+                Some(match str.chars().nth(0).unwrap() {
+                    '+' => { (Operation::Add, str[1..].parse::<i16>().unwrap()) }
+                    '-' => { (Operation::Subst, str[1..].parse::<i16>().unwrap()) }
+                    '*' => { (Operation::Mul, str[1..].parse::<i16>().unwrap()) }
+                    '/' => { (Operation::Div, str[1..].parse::<i16>().unwrap()) }
+                    _ => { return None }
+                })
+            }).collect()
+        }
+    };
+
+    Some(Expression {
+        number: match result.name("num") {
+            Some(m) => {
+                if m.as_str().len() != 0 {
+                    m.as_str().parse::<u8>().unwrap()
+                } else {
+                    1u8
+                }
+            }
+            None => { 1u8 }
+        },
+        dice: result.name("dice").unwrap().as_str().parse::<u8>().unwrap(),
+        modifiers: match mods_option {
+            None => { vec![] }
+            Some(mods) => { mods }
+        },
+    })
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,17 +1,33 @@
 #[cfg(test)]
 mod tests {
-    use crate::{roll, Expression, Operation};
-    use crate::Dice::D10;
+    use crate::{roll, Expression, Operation, parse, roll_str};
 
     #[test]
     fn it_works() {
         let result = roll(
             &Expression {
                 number: 5,
-                dice: D10,
+                dice: 10,
                 modifiers: vec![(Operation::Add, 5)],
             }).unwrap();
         println!("{}", result);
         assert_eq!(result.value.len(), 5);
+    }
+
+    #[test]
+    fn parse_test() -> Result<(), ()> {
+        let expr = parse("5d10+5").unwrap();
+        println!("{:?}", expr);
+        Ok(())
+    }
+
+    #[test]
+    fn roll_str_test() {
+        let result1 = roll_str("5d10+5").unwrap();
+        let result2 = roll_str("d10+5").unwrap();
+        let result3 = roll_str("5d10").unwrap();
+        assert_eq!(result1.value.len(), 5);
+        assert_eq!(result2.value.len(), 1);
+        assert_eq!(result3.value.len(), 5);
     }
 }
